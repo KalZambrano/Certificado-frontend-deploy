c08892a1a58c467010ffd18d246c946eb2f19e62
Kaleb <kalebzambrano@outlook.com>
2025-06-19 20:51:31 -0500

    hiding console

--- src/components/FetchCertificates.tsx
+++ src/components/FetchCertificates.tsx
@@ -33,7 +33,7 @@ export function FetchCertificates() {
                 return response.json();
             })
             .then(certificates => {
-                console.log(certificates);
+                // console.log(certificates);
                 setData(certificates);
                 setLoading(false);
             })
