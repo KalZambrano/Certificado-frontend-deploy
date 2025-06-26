d1f5d5e1e46e48f9a81e456f3cfc73835fed47e2
Kaleb <kalebzambrano@outlook.com>
2025-06-26 14:07:40 -0500

    adding fields for description and skills

--- src/components/VerificationPage.tsx
+++ src/components/VerificationPage.tsx
@@ -3,7 +3,19 @@ import { useEffect, useState, useRef } from "react";
 import { loadExternalScript } from "@/lib/localExternalScript";
 
 export function VerificationPage() {
-    const [certificate, setCertificate] = useState(null);
+    interface CertificateAPI {
+        id:                 number;
+        nombreEstudiante:   string;
+        codigo:             null;
+        curso:              string;
+        nota:               number;
+        fechaEmision:       Date;
+        codigoVerificacion: string;
+        habilidades:        string[];
+        descripcion:        string;
+    }
+
+    const [certificate, setCertificate] = useState<CertificateAPI | null>(null);
     const [loading, setLoading] = useState(true)
 
     const canvasRef = useRef(null);
@@ -45,16 +57,30 @@ export function VerificationPage() {
     if (loading) { return <p className="grid min-h-screen place-content-center text-5xl font-extrabold animate-pulse bg-blue-200 text-black">Cargando...</p> }
 
     if (!certificate) { return <p className="grid min-h-screen place-content-center text-5xl font-extrabold bg-blue-200 text-black">Certificado no encontrado...</p> }
+    console.log(certificate)
 
-    const { nombreEstudiante, nota, fechaEmision, curso } = certificate
+    const { nombreEstudiante, nota, fechaEmision, curso, habilidades, descripcion } = certificate
 
     return (
         <>
-            <div className="grid min-h-screen place-content-center text-5xl font-extrabold bg-blue-200 text-black">
+            <div className="grid min-h-screen place-content-center text-5xl font-extrabold text-black max-w-6xl mx-auto">
                 <h1>Certificado de finalización</h1>
                 <p>Nombre del estudiante: {nombreEstudiante}</p>
                 <p>Curso: {curso}</p>
                 <p>Nota: {nota}</p>
+                <p>{descripcion}</p>
+                {
+                    habilidades && habilidades.length > 0 && (
+                        <div>
+                            <p className="text-2xl mb-4">Habilidades adquiridas</p>
+                            <ul className="list-disc pl-5">
+                                {habilidades.map((habilidad, index) => (
+                                    <li className="first-letter:uppercase lowercase" key={index}>{habilidad}</li>
+                                ))}
+                            </ul>
+                        </div>
+                    )
+                }
                 <p>Fecha de emisión: {new Date(fechaEmision).toLocaleDateString()}</p>
                 <div className="mt-8">
                     <p className="text-2xl mb-4">Codigo de Prueba</p>
